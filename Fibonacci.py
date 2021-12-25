c079cbd666075fe4d777c178d9cb4b5f5d4881f2
W-Gekko <62272491+wgekko@users.noreply.github.com>
2021-12-25 01:48:46 -0300

    Add files via upload

--- Fibonacci.py
+++ Fibonacci.py
@@ -18,36 +18,82 @@ def fibonacci(n):
         return 1
     return (fibonacci(n-1)+ fibonacci(n-2))
 
-try:
-    r= int(input("ingrese la cantidad de veces que desea llamar la funcion de fibonacci : "))
-    assert r>0
-except AssertionError:
-    print("ERROR, debe ingresar un valor positivo ...")
-    dato=input("desea continuar con el cálculo de fibonacci SI = s / NO = n : ")
-  
-    if dato.lower == 'n':
-        print("salió correctamente del programa ...")
-    else:
-        r= int(input("ingrese la cantidad de veces que desea llamar la funcion de calculo de fibonacci : "))
-    
-print("el numero es :", fibonacci(r), " la función de cálculo de Fibonacci fue llamada :", r)
-#print(f"fibonacci() ha sido llamada  {nro:,} veces" )
-
-#-------------- fibonacci ---------
-# ------ mostrar la secuencia -------
-#--desarrollo con recursion -----------
-
 def recur_fibonacci(n):
    if n <= 1:
        return n
    else:
        return(recur_fibonacci(n-1) + recur_fibonacci(n-2))
 
-secuencia = r
+#while True:   
+   
+try:
+         r= int(input("ingrese la cantidad de veces que desea llamar la funcion de fibonacci : "))
+         assert r>0
+except AssertionError:
+        print("ERROR, debe ingresar un valor positivo ...")   
+    
+finally: 
+        dato=input("desea continuar con el cálculo de fibonacci SI = s / NO = n : ")
+  
+        if dato.lower() =="s":
+            r= int(input("ingrese la cantidad de veces que desea llamar la funcion de calculo de fibonacci : ")) 
+            print("el numero es :", fibonacci(r), " la función de cálculo de Fibonacci fue llamada :", r)
+            secuencia = r
+            print("la secuencia de Fibonacci de números calculados es: ")
+            for i in range(secuencia):
+                print(recur_fibonacci(i))      
+                
+        elif dato.lower()=="n":        
+            print("salió correctamente del programa ...")      
+         
 
-print("la secuencia de Fibonacci de números calculados es: ")
-for i in range(secuencia):
-       print(recur_fibonacci(i))
+
+
+#
+#try:
+#    r= int(input("ingrese la cantidad de veces que desea llamar la funcion de fibonacci : "))
+#    assert r>0
+#except AssertionError:
+#    print("ERROR, debe ingresar un valor positivo ...")   
+#    
+#    dato=input("desea continuar con el cálculo de fibonacci SI = s / NO = n : ")
+#  
+#    if dato.lower() =="s":
+#          r= int(input("ingrese la cantidad de veces que desea llamar la funcion de calculo de fibonacci : ")) 
+#          print("el numero es :", fibonacci(r), " la función de cálculo de Fibonacci fue llamada :", r)
+#          secuencia = r
+#          print("la secuencia de Fibonacci de números calculados es: ")
+#          for i in range(secuencia):
+#              print(recur_fibonacci(i))
+#        
+#                
+#    elif dato.lower()=="n":        
+#        print("salió correctamente del programa ...")      
+    
+#print("el numero es :", fibonacci(r), " la función de cálculo de Fibonacci fue llamada :", r)
+#secuencia = r
+#print("la secuencia de Fibonacci de números calculados es: ")
+#for i in range(secuencia):
+#    print(recur_fibonacci(i))
+        
+        
+#print(f"fibonacci() ha sido llamada  {nro:,} veces" )
+
+#-------------- fibonacci ---------
+# ------ mostrar la secuencia -------
+#--desarrollo con recursion -----------
+
+#def recur_fibonacci(n):
+#   if n <= 1:
+#       return n
+#   else:
+#       return(recur_fibonacci(n-1) + recur_fibonacci(n-2))
+#
+#secuencia = r
+#
+#print("la secuencia de Fibonacci de números calculados es: ")
+#for i in range(secuencia):
+#       print(recur_fibonacci(i))
 
 # validar si el numero es valido 
 #if secuencia <= 0:
